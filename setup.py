46ac1dd44583ba7352edf207b6f13bddc5b7083b
unknown <dalagnolbruno88@gmail.com>
2022-11-14 11:35:30 -0300

    adicionando info para o projeto

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, Extension
 ext = Extension('benchtools', sources=["main.c", "benchmark.c"])
 setup(
+      name="BenchTools",
+      version="1.0.0-b0",
+      description="Is an simple bench tool.",
+      author="Bruno Dalagnol",
+      author_email="dalagnolbruno88@gmail.com",
+      license="MIT",
+      url="https://github.com/PothpothBR/benchtools",
       ext_modules=[ext]
 )
