9c44b650c191af5a2c70e405807da76702b00e02
unknown <dalagnolbruno88@gmail.com>
2022-11-15 15:04:15 -0300

    formalizando readme para pypi

--- setup.py
+++ setup.py
@@ -12,12 +12,23 @@ ext = Extension(
 
 setup(
       name="BenchTools",
-      version="1.0.3.rev2",
+      version="1.0.3.rev3",
       description="Is an simple bench tool.",
       author="Bruno Dalagnol",
       author_email="dalagnolbruno88@gmail.com",
       license="MIT",
       url="https://github.com/PothpothBR/benchtools",
+      python_requires=">=3.0",
+      keywords=[
+            "benchmark",
+            "optimization",
+            "bench tools",
+            "benchmark tool"
+      ],
+      platforms=[
+            "Windows",
+            "Linux"
+      ],
       ext_modules=[ext],
       long_description=long_description,
       long_description_content_type='text/markdown'
